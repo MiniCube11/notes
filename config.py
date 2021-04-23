d0526912bc9bb749ef74f7fb1e8da25b43f36b0e
MiniCube11 <neanderthal00011@gmail.com>
2021-04-22 21:26:03 -0400

    Update database url

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 SECRET_KEY = os.environ.get("SECRET_KEY")
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
