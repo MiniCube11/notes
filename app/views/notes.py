6510716757e7a006c67c6d0ec338e81d8ae7b4ca
MiniCube11 <neanderthal00011@gmail.com>
2021-06-11 16:29:21 -0400

    Update /notes routes

--- app/views/notes.py
+++ app/views/notes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, redirect, render_template, url_for, request, abort, flash, session
-from app import app, db
+from app import db
 from app.models import Note
 from flask_login import current_user, login_required
 from datetime import datetime
@@ -7,48 +7,63 @@ from datetime import datetime
 bp = Blueprint('notes', __name__, url_prefix='/notes')
 
 
-@bp.route('/new')
-@login_required
-def add_note():
+def create_note():
     note = Note(title="Untitled", body='', author=current_user)
     note.set_key()
     db.session.add(note)
     db.session.commit()
-    return redirect(url_for('notes.edit_note', key=note.key))
+    return note.key
 
 
-@bp.route('/<key>/edit', methods=['GET', 'POST'])
-def edit_note(key):
-    note = Note.query.filter_by(key=key).first_or_404()
-    if request.method == "POST":
-        title = request.form.get("title")
-        body = request.form.get("body")
-        note.title = title
-        note.body = body
-        note.last_updated = datetime.utcnow()
-        db.session.commit()
-    if note.author == current_user:
-        return render_template('notes/edit_note.html', note=note)
-    elif note.is_public:
-        return render_template('notes/view_note.html', note=note)
-    else:
-        if current_user.is_authenticated:
-            if current_user.is_admin:
-                return render_template('notes/edit_note.html', note=note)
-            else:
-                abort(403)
-        else:
-            session['next'] = url_for('notes.edit_note', key=key)
-            return redirect(url_for('auth.login'))
+def update_note(note):
+    title = request.form.get("title")
+    body = request.form.get("body")
+    note.title = title
+    note.body = body
+    note.last_updated = datetime.utcnow()
+    db.session.commit()
 
 
-@bp.route('/<key>/delete', methods=['POST'])
-def delete_note(key):
-    note = Note.query.filter_by(key=key).first()
+def delete_note(note):
     flash(f"The note '{note.title}' has been deleted.")
     db.session.delete(note)
     db.session.commit()
-    return "Done"
+
+
+@bp.route('/new')
+@login_required
+def new():
+    note_key = create_note()
+    return redirect(url_for('notes.get_note', key=note_key))
+
+
+@bp.route('/<key>/edit')
+def old_edit_note(key):
+    return redirect(url_for('notes.edit_note', key=key))
+
+
+@bp.route('/api/<key>', methods=['GET', 'POST', 'DELETE'])
+def edit_note(key):
+    note = Note.query.filter_by(key=key).first_or_404()
+    if request.method == "GET":
+        return {'title': note.title, 'body': note.body}
+    allowed = False
+    if note.is_public:
+        allowed = True
+    if current_user.is_authenticated:
+        if current_user.is_admin or note.author == current_user:
+            allowed = True
+    if not allowed:
+        if not current_user.is_authenticated:
+            return "Unauthorized", 401
+        else:
+            return "Forbidden", 403
+    if request.method == "POST":
+        update_note(note)
+        return "done"
+    elif request.method == "DELETE":
+        delete_note(note)
+        return "done"
 
 
 @bp.route('/<key>/public', methods=['POST'])
@@ -62,7 +77,19 @@ def toggle_public(key):
     return str(note.is_public)
 
 
-@bp.route('/<key>/get', methods=['POST'])
+@bp.route('/<key>')
 def get_note(key):
-    note = Note.query.filter_by(key=key).first()
-    return {'title': note.title, 'body': note.body}
+    note = Note.query.filter_by(key=key).first_or_404()
+    if note.author == current_user:
+        return render_template('notes/edit_note.html', note=note)
+    elif note.is_public:
+        return render_template('notes/view_note.html', note=note)
+    else:
+        if current_user.is_authenticated:
+            if current_user.is_admin:
+                return render_template('notes/edit_note.html', note=note)
+            else:
+                abort(403)
+        else:
+            session['next'] = url_for('notes.edit_note', key=key)
+            return redirect(url_for('auth.login'))
