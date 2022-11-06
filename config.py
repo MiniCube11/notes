f241c21335ad9e110f2de7ceaf8ea38d3187e4db
Ching Lam Lau <contact.minicube11@gmail.com>
2022-11-05 22:59:40 -0400

    Update config file

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
 
-if os.environ.get("TESTING"):
-    REDIRECT_URI = "http://127.0.0.1:5000"
-else:
-    REDIRECT_URI = "https://notesapp0011.herokuapp.com"
+REDIRECT_URI = os.environ.get('REDIRECT_URI')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("://", "ql://", 1)
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 ADMIN_EMAIL = os.environ.get("ADMIN_EMAIL")
