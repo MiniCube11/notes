060cfdb1cc0c7059737ef56700da4ff3fec6d460
Ching Lam Lau <contact.minicube11@gmail.com>
2024-12-12 18:16:30 -0500

    Add search functionality

--- app/views/main.py
+++ app/views/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 from flask_login import current_user
 from app.models import Note
 from app import app, db
@@ -13,5 +13,10 @@ def index():
     if current_user.email == app.config['ADMIN_EMAIL']:
         current_user.is_admin = True
         db.session.commit()
+    query = request.args.get('q', '').lower()
     notes = Note.query.filter_by(author=current_user).order_by(Note.last_updated.desc()).all()
-    return render_template('main/index.html', notes=notes)
+    filtered_notes = []
+    for note in notes:
+        if query in note.title.lower() or query in note.body.lower():
+            filtered_notes.append(note)
+    return render_template('main/index.html', notes=filtered_notes, query=query)
